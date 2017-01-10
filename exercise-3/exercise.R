3eb11b6ca67a7267dd9f3c0723f40b30b0b0aacf
Jack Sinclair <jackjohnsinclair@gmail.com>
2017-01-10 15:19:47 -0800

    Worked on ecercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,25 +1,25 @@
 # Exercise-3: Calling built-in functions
 
 # Create a variable `my.name` that contains your name
-
+my.name <- "Jack"
 
 # Create a variable `name.length` that holds how many letters are in your name (use the `nchars()` function)
-
+name.length <- nchar("Jack")
 
 # Print the number of letters in your name
-
+print (name.length)
 
 # Create a variable `now.doing` that is your named followed by "is programming!" to the end of your name
 # (use the `paste()` function)
-
+now.doing <- paste(my.name, "is programming!")
 
 # Make the `now.doing` variable upper case
-
+toupper(now.doing)
 
 ### Bonus
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to `fav.1` and `fav.2`
-
+fav.1 <- 5
 
 # Divide each number by the square root of 201 and save it as the same variable
 
